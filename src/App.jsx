910434b88e788ca1ce25e2e7802d8c0d70267dc0
jjdavenport <jordanjamesdavenport@gmail.com>
2024-12-31 21:23:49 +0000

    add custom app hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,25 @@
-import { useRef, useState } from "react";
 import data from "./assets/data.json";
 import Form from "./components/form.jsx";
 import Progress from "./components/progress.jsx";
 import Buttons from "./components/buttons.jsx";
-import { useMediaQuery } from "react-responsive";
 import Footer from "./components/footer.jsx";
+import useApp from "./hooks/useApp.jsx";
 
 function App() {
-  const formRef = useRef(null);
-  const [page, setPage] = useState(1);
-  const [plan, setPlan] = useState({
-    plan: data.steps[1].plans[0].name,
-    price: data.steps[1].plans[0].priceM,
-  });
-  const [yearly, setYearly] = useState(false);
-  const [valid, setValid] = useState(false);
-  const desktop = useMediaQuery({ minWidth: 768 });
-
-  const changePlan = () => {
-    setPage(2);
-  };
-
-  const nextPage = () => {
-    if (formRef.current) {
-      formRef.current.requestSubmit();
-    }
-    setValid((prev) => {
-      if (prev) {
-        setPage((prevPage) => prevPage + 1);
-      }
-      return prev;
-    });
-  };
-
-  const prevPage = () => {
-    setPage((prev) => prev - 1);
-  };
-
-  const dataProp = () => {
-    return page === 1
-      ? data.steps[0]
-      : page === 2
-        ? data.steps[1]
-        : page === 3
-          ? data.steps[2]
-          : data.steps[3];
-  };
+  const {
+    formRef,
+    page,
+    plan,
+    setPlan,
+    yearly,
+    setYearly,
+    setValid,
+    desktop,
+    changePlan,
+    nextPage,
+    prevPage,
+    dataProp,
+  } = useApp({ data });
 
   return (
     <>
---
+++ src/hooks/useApp.jsx
@@ -0,0 +1,62 @@
+import { useRef, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+
+const useApp = ({ data }) => {
+  const formRef = useRef(null);
+  const [page, setPage] = useState(1);
+  const [plan, setPlan] = useState({
+    plan: data.steps[1].plans[0].name,
+    price: data.steps[1].plans[0].priceM,
+  });
+  const [yearly, setYearly] = useState(false);
+  const [valid, setValid] = useState(false);
+  const desktop = useMediaQuery({ minWidth: 768 });
+
+  const changePlan = () => {
+    setPage(2);
+  };
+
+  const nextPage = () => {
+    if (formRef.current) {
+      formRef.current.requestSubmit();
+    }
+    setValid((prev) => {
+      if (prev) {
+        setPage((prevPage) => prevPage + 1);
+      }
+      return prev;
+    });
+  };
+
+  const prevPage = () => {
+    setPage((prev) => prev - 1);
+  };
+
+  const dataProp = () => {
+    return page === 1
+      ? data.steps[0]
+      : page === 2
+        ? data.steps[1]
+        : page === 3
+          ? data.steps[2]
+          : data.steps[3];
+  };
+
+  return {
+    formRef,
+    page,
+    plan,
+    setPlan,
+    yearly,
+    setYearly,
+    valid,
+    setValid,
+    desktop,
+    changePlan,
+    nextPage,
+    prevPage,
+    dataProp,
+  };
+};
+
+export default useApp;
